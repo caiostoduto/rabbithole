11842882ba9275c963ddc9afc9ba900007e64f92
Caio Stoduto <caio.stoduto@outlook.com>
2024-01-20 02:16:53 -0300

    feat: create pre-signed cloudflare r2 upload

--- functions/index.ts
+++ functions/index.ts
@@ -1,7 +1,13 @@
-interface Env {
-	RABBITHOLE: KVNamespace;
+export interface Env {
+	KV: KVNamespace;
+	BUCKET: R2Bucket;
+
+	ENDPOINT: string;
+	BUCKETNAME: string;
+	ACCESSKEYID: string;
+	SECRETACCESSKEY: string;
 }
 
 export const onRequest: PagesFunction<Env> = async (context) => {
-	return Response.redirect(await context.env.RABBITHOLE.get("_redirect-url"));
+	return Response.redirect(await context.env.KV.get("_redirect-url"));
 };
