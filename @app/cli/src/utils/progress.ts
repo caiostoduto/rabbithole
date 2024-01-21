e2a8dc63c0aca0746824b0fdd1b37de1f7d9c947
Caio Stoduto <caio.stoduto@outlook.com>
2024-01-21 16:32:23 -0300

    feat(cli): add estimated time (ETA)

--- @app/cli/src/utils/progress.ts
+++ @app/cli/src/utils/progress.ts
@@ -4,8 +4,24 @@ function repeat(num: number, char: string): string {
   return [...new Array(Math.round(num))].map(() => char).join('');
 }
 
-export function progressBar(percentage: number, size: number = 40) {
+function formatTime(t: number) {
+  return t >= 10 ? t : `0${t}`
+}
+
+function calcETA(percentage: number, start: Date,) {
+  const elapsed = new Date().getTime() - start.getTime()
+
+  const estimated = percentage > 0 ? elapsed / (percentage / 1e2) : 0
+  const remaining = Math.round((estimated - elapsed) / 1e3)
+
+  return (`${formatTime(Math.floor(remaining / 60))}:${formatTime(remaining - (Math.floor(remaining / 60) * 60))}`)
+}
+
+export function progressBar(percentage: number, start: Date, size: number = 40) {
   const track = repeat(size, "░");
   const completed = repeat((percentage / 100) * size, "█");
-  return `${completed}${track}`.substring(0, size) + ` ${percentage}%`;
+
+  const eta = calcETA(percentage, start)
+
+  return `${completed}${track}`.substring(0, size) + ` ${percentage}% | ETA ${eta}`;
 }
